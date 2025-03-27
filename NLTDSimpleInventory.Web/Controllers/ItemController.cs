8b3904472af2ec376dde6d45ff791ef5eb72ff56
Jewel Binueza <binuezajewel@gmail.com>
2025-03-27 17:20:41 +0800

    Item fetch and display

--- NLTDSimpleInventory.Web/Controllers/ItemController.cs
+++ NLTDSimpleInventory.Web/Controllers/ItemController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NLTDSimpleInventory.DataLayer.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace NLTDSimpleInventory.Web.Controllers
+namespace NLTDSimpleInventory.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly SimpleInventoryContext _context;
+
+        public ItemController(SimpleInventoryContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var items = _context.Items.ToList(); 
+            return View(items);
         }
     }
 }
